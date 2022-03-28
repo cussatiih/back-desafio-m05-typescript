3f2d8592669045f2b38c9f018ecb8a17f87fd0a7
cussatiih <cussatiih@gmail.com>
2022-03-28 07:30:55 -0300

    User show NaN handling

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -25,6 +25,8 @@ export default class UserController {
   async show(req: Request, res: Response): Promise<Response> {
     const id = Number(req.params.id);
 
+    if (isNaN(id))
+      return res.status(400).json({ error: "ID parameter must be a number." });
     try {
       const user = await UserService.show(id);
 
